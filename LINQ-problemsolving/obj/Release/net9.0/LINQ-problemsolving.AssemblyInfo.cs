e23e6f3a7a2d231507b43f855ad25931d0eeff03
Fatma Gomea <Fatma.Gomea@ELSEWEDY-SCHOOL.LOCAL>
2026-03-11 13:51:02 +0200

    add comment

--- LINQ-problemsolving/obj/Release/net9.0/LINQ-problemsolving.AssemblyInfo.cs
+++ LINQ-problemsolving/obj/Release/net9.0/LINQ-problemsolving.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ-problemsolving")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708cb4613c0b120337ee6b3630d82926923d0235")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ-problemsolving")]
