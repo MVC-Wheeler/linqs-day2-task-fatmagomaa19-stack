f709f6ecfac037ef0e9807f7af46584e1e1c5618
Fatma Gomea <Fatma.Gomea@ELSEWEDY-SCHOOL.LOCAL>
2026-03-11 13:15:30 +0200

    Task 2

---
+++ LINQ-problemsolving/Enrollment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_problemsolving
+{
+    internal class Enrollment
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        //public Enrollment(int studentId, int courseId)
+        //{
+        //    StudentId = studentId;
+        //    CourseId = courseId;
+        //}
+        public override string ToString()
+        {
+            return $"StudentId : {StudentId},CourseId : {CourseId}";
+        }
+
+    }
+
+}
+
